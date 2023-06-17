bccecc3403e0101c4a469fe7d78c5204fd2d18ff
vahidesh <104305474+vahidesh@users.noreply.github.com>
2023-06-17 12:32:51 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿//A meselesi
+/*
+int count = 68;
+
+if (count >= 88 && count <= 100)
+{
+    Console.WriteLine("Success - AA");
+
+}
+else if (count >= 81 && count < 88)
+{
+    Console.WriteLine("Success - BA");
+}
+else if (count >= 74 && count < 81)
+{
+    Console.WriteLine("Success - BB");
+}
+else if (count >= 67 && count < 74)
+{
+    Console.WriteLine("Success - CB");
+}
+else if (count >= 60 && count < 67)
+{
+    Console.WriteLine("Success - CC");
+}
+else if (count >= 0 && count < 60)
+{
+    Console.WriteLine("Fail - FF");
+}
+else
+{
+    Console.WriteLine("Invalid score");
+}
+
+Console.WriteLine("Bitdi.");
+*/
+
+
+//B meselesi
+/*
+string command = "stop";
+
+switch (command)
+{
+    case "start":
+        Console.WriteLine("Game is starting");
+        break;
+    case "stop":
+        Console.WriteLine("Game stopped");
+        break;
+    case "wait":
+        Console.WriteLine("Game paused");
+        break;
+
+    default:
+        Console.WriteLine("Command not found");
+        break ;
+       
+}
+*/
+
+
+//C meselesi
+/*
+int value = 5;
+int count = 1;
+int sum = 0;
+
+while (count <= value)
+{
+    sum += count;
+    count += 1;
+}
+
+Console.WriteLine(sum);
+*/
+
+
+
+//D meselesi
+/*
+int count = 1;
+
+while (count <=100)
+{
+    if (count % 3 == 0 || count % 5 == 0)
+    {
+        Console.WriteLine(count);
+    }
+    count += 1;
+}
+*/
+
